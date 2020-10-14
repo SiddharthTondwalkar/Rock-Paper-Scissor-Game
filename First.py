0c889b496e88fa6453062632310b9cf0ccdc1e7d
Sidhhu58 <58626548+Sidhhu58@users.noreply.github.com>
2020-10-14 20:27:39 +0530

    Update

--- First.py
+++ First.py
@@ -1,7 +1,9 @@
 import random
 
-
-t = eval (input("How Much Time You Have To Play :"))
+try:
+    t = eval (input("How Much Time You Have To Play :"))
+except Exception as e:
+    print(e)
 
 player_win = 0
 Comp_win = 0
@@ -21,52 +23,55 @@ def Comp_in():
     comp = ['r','p', 's']
     c = random.choice(comp)
     return c 
+try:
+    for i in range(1,t+1):
+        print("Rock, Paper, Scissor")
+        P = Player_in()
+        c = Comp_in()
+        if P == "r":
+            if c == "p":
+                print("You Lost You has Choose Rock and computer had Choose paper")
+                Comp_win +=1    
+                    
+        if P == "p":
+            if c == "s":
+                print("You Lost You has Choose Paper and computer had Choose Scissor ")
+                Comp_win +=1    
 
-for i in range(1,t+1):
-    print("Rock, Paper, Scissor")
-    P = Player_in()
-    c = Comp_in()
-    if P == "r":
-        if c == "p":
-            print("You Lost You has Choose Rock and computer had Choose paper")
-            Comp_win +=1    
+        if P == "s":
+            if c == "r":
+                print("You Lost You has Choose Scissor and computer had Choose Rock ")
+                Comp_win +=1
+        if P == "r":
+            if c == "r":
+                print("Tie You and Computer Has chosed Rock")
+
+        if P == "s":
+            if c == "s":
+                print("Tie You and Computer Has chosed Scissor")
             
-    if P == "p":
-        if c == "s":
-            print("You Lost You has Choose Paper and computer had Choose Scissor ")
-            Comp_win +=1    
+        if P == "p":
+            if c == "p":
+                print("Tie You and Computer Has chosed Paper")
 
-    if P == "s":
-        if c == "r":
-            print("You Lost You has Choose Scissor and computer had Choose Rock ")
-            Comp_win +=1
-    if P == "r":
-        if c == "r":
-            print("Tie You and Computer Has chosed Rock")
+        if P == "r":
+            if c == "s":
+                print("You Won You has Choose Rock and computer had Choose Scissor")
+                player_win +=1    
+                    
+        if P == "s":
+            if c == "p":
+                print("You Won You had Choose Scissor and computer had Choose Paper ")
+                player_win +=1    
 
-    if P == "s":
-        if c == "s":
-            print("Tie You and Computer Has chosed Scissor")
-     
-    if P == "p":
-        if c == "p":
-            print("Tie You and Computer Has chosed Paper")
-
-    if P == "r":
-        if c == "s":
-            print("You Won You has Choose Rock and computer had Choose Scissor")
-            player_win +=1    
+        if P == "p":
+            if c == "r":
+                print("You won You has Choose Paper and computer had Choose Rock ")
+                player_win +=1
             
-    if P == "s":
-        if c == "p":
-            print("You Won You had Choose Scissor and computer had Choose Paper ")
-            player_win +=1    
-
-    if P == "p":
-        if c == "r":
-            print("You won You has Choose Paper and computer had Choose Rock ")
-            player_win +=1
-    
-    print("Total Score")
-    print("Computer Score :"+str(Comp_win))
-    print("Player Score :"+str(player_win))
+        print("Total Score")
+        print("Computer Score :"+str(Comp_win))
+        print("Player Score :"+str(player_win))
+except Exception as e:
+    print(e)
+        
